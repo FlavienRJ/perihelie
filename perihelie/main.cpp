cf9c9c662daab32d8a357e041ff807e1bd59cdb5
flexus404 <flavien.trade18@gmail.com>
2016-07-13 20:55:05 +0200

    ajout des premiers fichiers du projet

---
+++ perihelie/main.cpp
@@ -0,0 +1,13 @@
+/*
+ Programme de modélisation de la relativité générale
+ 1-Précession du périhélie
+ 2-Système à 2 corps
+ */
+
+#include <iostream>
+
+int main(int argv, char const** argc)
+{
+	
+    return 0;
+}
